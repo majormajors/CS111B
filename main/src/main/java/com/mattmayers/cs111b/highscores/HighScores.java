48369592a751aff9bbb266353a85a72e51abe421
Matthew Mayers <majormajors@users.noreply.github.com>
2019-10-04 08:30:30 -0700

    Add selection sort

--- main/src/main/java/com/mattmayers/cs111b/highscores/HighScores.java
+++ main/src/main/java/com/mattmayers/cs111b/highscores/HighScores.java
@@ -24,30 +24,45 @@ public class HighScores {
         in.close();
     }
 
-    /**
-     * Simple bubble sort
-     * @param names
-     * @param scores
-     */
     private static void sort(ArrayList<String> names, ArrayList<Integer> scores) {
-        int swaps;
+        selectionSort(names, scores);
+    }
+
+    private static void bubbleSort(ArrayList<String> names, ArrayList<Integer> scores) {
+        boolean swap;
         do {
-            swaps = 0;
+            swap = false;
             for (int i = 0; i < scores.size()-1; i++) {
-                int currentScore = scores.get(i);
-                int nextScore = scores.get(i+1);
-                if (currentScore < nextScore) {
-                    scores.set(i, nextScore);
-                    scores.set(i+1, currentScore);
-
-                    String tempName = names.get(i);
-                    names.set(i, names.get(i+1));
-                    names.set(i+1, tempName);
-
-                    swaps++;
+                if (scores.get(i) < scores.get(i+1)) {
+                    swap(names, scores, i, i+1);
+                    swap = true;
                 }
             }
-        } while (swaps > 0);
+        } while (swap);
+    }
+
+    private static void selectionSort(ArrayList<String> names, ArrayList<Integer> scores) {
+        final int length = scores.size();
+        for (int i = 0; i < length-1; i++) {
+            int jmax = i;
+            for (int j = i + 1; j < length; j++) {
+                if (scores.get(j) > scores.get(jmax)) {
+                    jmax = j;
+                }
+            }
+            if (jmax != i) {
+                swap(names, scores, i, jmax);
+            }
+        }
+    }
+
+    private static void swap(ArrayList<String> names, ArrayList<Integer> scores, int i1, int i2) {
+        final String nameTemp = names.get(i1);
+        final Integer scoreTemp = scores.get(i1);
+        names.set(i1, names.get(i2));
+        scores.set(i1, scores.get(i2));
+        names.set(i2, nameTemp);
+        scores.set(i2, scoreTemp);
     }
 
     private static void display(ArrayList<String> names, ArrayList<Integer> scores) {
